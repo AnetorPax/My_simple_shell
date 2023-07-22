85678f484f0050b7f47e2f950703d41c4c5ee009
AnetorPax <anetorpax@gmail.com>
2023-07-22 14:18:19 +0100

    TAsk 0 and Task 1

--- simple_shell_1.c
+++ simple_shell_1.c
@@ -5,24 +5,39 @@
 
 #define _GNU_SOURCE
 
+
+/**
+ * exec_command - Executes the provided command using execve.
+ * @command: The command to execute.
+ *
+ * Return: Nothing
+ */
+void exec_command(char *command)
+{
+	char *const argv[] = {command, NULL};
+
+	if (execve(command, argv, NULL) == -1)
+	{
+		perror("execve error");
+		exit(EXIT_FAILURE);
+	}
+}
+
 /**
  * main - the beginning of the program
  * Return: 0 Always Success
  */
 int main(void)
 {
-	char *line = NULL;
+	char *line = NULL, *prompt = "#cisfun$ ";
 	size_t len = 0;
 	ssize_t read;
-	char *prompt = "#cisfun$ ";
 	int status;
 	pid_t pid;
-	char *const argv[] = {line, NULL};
 
 	while (1)
 	{
 		printf("%s", prompt);
-
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 		{
@@ -30,9 +45,7 @@ int main(void)
 				putchar('\n');
 			break;
 		}
-
 		line[read - 1] = '\0';
-
 		pid = fork();
 		if (pid == -1)
 		{
@@ -40,13 +53,7 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 		else if (pid == 0)
-		{
-			if (execve(line, argv, NULL) == -1)
-			{
-				perror("execve error");
-				exit(EXIT_FAILURE);
-			}
-		}
+			exec_command(line);
 		else
 		{
 			if (wait(&status) == -1)
@@ -56,8 +63,6 @@ int main(void)
 			}
 		}
 	}
-
 	free(line);
-
-	return(0);
+	return (0);
 }
